9493d4120f8084fd1932468a7d681b9859936680
kuma88 <kumarasond@gmail.com>
2021-01-31 21:09:03 +0800

    added form

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React, {useState} from "react";
 import {View, StyleSheet, FlatList} from "react-native";
-import ColorButton from "./components/ColorButton"
-import defaultColors from "./data/defaultColors.json"
+import ColorButton from "./components/ColorButton";
+import defaultColors from "./data/defaultColors.json";
+import ColorForm from "./components/ColorForm";
 
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("black");
   return (
-    <FlatList style={[styles.container, {backgroundColor}]}
-      data={defaultColors}
-      renderItem={({item}) => {
-        return (
-          <ColorButton key={item.id}
-            backgroundColor={item.color}
-            onPress={setBackgroundColor} 
-          />
-        );
-      }}
-    />
+    <>
+      <ColorForm/>
+      <FlatList style={[styles.container, {backgroundColor}]}
+        data={defaultColors}
+        renderItem={({item}) => {
+          return (
+            <ColorButton key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor} 
+            />
+          );
+        }}
+      /> 
+    </>
   );
 }
 
